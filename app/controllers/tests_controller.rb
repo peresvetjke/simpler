9fa13177bf3c8986f7c6c45fc3dbeec27583c022
peresvetjke <igor.udalov@gmail.com>
2021-11-04 13:37:11 +0300

    Add corrections

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,13 @@
 class TestsController < Simpler::Controller
 
+  def show
+    set_header('Content-Language', 'en')
+    # render plain: "Show time! params ARE: #{params}", status: 201
+  end
+
   def index
     @time = Time.now
+    render plain: "Index here! params ARE: #{params}"
   end
 
   def create
